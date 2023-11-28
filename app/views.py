e4cd98623aa2149d17d50bdb14de8661ec343406
eunhabaek <beh2549@gmail.com>
2023-11-28 11:40:15 +0900

    action yml 파일 수정

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+def index(request):
+    return render(request, 'index.html')
+
+from django.contrib import admin
+from django.urls import path
+from .views import index
+urlpatterns = [
+path("", index),]
+
+
